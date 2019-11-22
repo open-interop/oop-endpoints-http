096d9fe814a743996dae005a809659cb27d93b9c
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-11-22 10:17:08 +0000

    fix: robustifying endpoint

--- main.test.js
+++ main.test.js
@@ -3,15 +3,21 @@ import test from "ava";
 const mock = require("mock-require");
 const fetchMock = require("fetch-mock");
 
-mock(
-    "node-fetch",
-    fetchMock.sandbox().mock("http://example.com", { body: "Hello, World!" })
-);
+const sandbox = fetchMock.sandbox();
+
+sandbox.mock("http://example.com", { body: "Hello, World!" });
+sandbox.catch(async () => {
+    throw new Error("Something went wrong.");
+});
+
+mock("node-fetch", sandbox);
 
 const main = require("./main");
 
+const mockConsole = { info: () => {}, log: () => {}, error: () => {} };
+
 test("request works", async t => {
-    t.plan(2);
+    t.plan(4);
 
     const p = new Promise(resolve => {
         const broker = {
@@ -28,9 +34,7 @@ test("request works", async t => {
                             body: "test"
                         },
                         tempr: {
-                            deviceTemprId: 1,
-                            queueRequest: true,
-                            queueResponse: true
+                            deviceTemprId: 1
                         },
                         rendered: {
                             host: "example.com",
@@ -46,15 +50,89 @@ test("request works", async t => {
                     nack: () => {}
                 });
             },
-            publish: (exchange, queue, response) => {
-                console.log(response);
-                t.is(response.requestBody, "test");
-                t.is(response.responseBody, "Hello, World!");
+            publish: (exchange, queue, message) => {
+                t.is(message.retries, undefined);
+                t.is(message.response.body, "Hello, World!");
+                t.is(message.response.headers["content-length"], "13");
+                t.is(message.response.status, 200);
+
                 resolve();
             }
         };
 
-        main(broker, {}, console);
+        main(
+            broker,
+            {
+                maxRetryAttempts: 0,
+                errorQ: "error.q",
+                coreResponseQ: "correct.q"
+            },
+            mockConsole
+        );
+    });
+
+    await p;
+});
+
+test("request retries then posts to error queue", async t => {
+    t.plan(3);
+
+    const p = new Promise(resolve => {
+        const broker = {
+            create: () => {},
+            consume: (queue, callback) => {
+                broker.callback = callback;
+                callback({
+                    content: {
+                        uuid: "000000-0000-0000-000000",
+                        transmissionId: 1,
+                        device: {
+                            id: 1
+                        },
+                        message: {
+                            body: "test"
+                        },
+                        tempr: {
+                            deviceTemprId: 1,
+                            queueRequest: true,
+                            queueResponse: true
+                        },
+                        rendered: {
+                            host: "other.com",
+                            port: "",
+                            path: "/",
+                            requestMethod: "GET",
+                            headers: {},
+                            body: "",
+                            protocol: "http"
+                        }
+                    },
+                    ack: () => {},
+                    nack: () => {}
+                });
+            },
+            publish: (exchange, queue, response) => {
+                broker.publish = (exchange, queue, response) => {
+                    t.is(exchange, "error.exchange");
+                    t.is(queue, "error.q");
+                    t.is(response.retries, 2);
+
+                    resolve();
+                };
+
+                broker.callback({ content: response });
+            }
+        };
+
+        main(
+            broker,
+            {
+                errorExchangeName: "error.exchange",
+                errorQ: "error.q",
+                maxRetryAttempts: 2
+            },
+            mockConsole
+        );
     });
 
     await p;
