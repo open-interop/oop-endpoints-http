7fb4403dabef005f853b72a6e12708bac65fb057
Daniel Young <daniel.young@bluefrontier.co.uk>
2020-07-21 16:53:14 +0100

    feat: fixing config variable error

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const oop = require("oop-node-common");
 
 module.exports = new oop.Config({
     amqpAddress: "OOP_AMQP_ADDRESS",
-    endpointsExchangeName: "OOP_ENDPOINTS_EXCHANGE_NAME",
+    exchangeName: "OOP_EXCHANGE_NAME",
     oopEndpointsQ: "OOP_ENDPOINT_Q",
     httpOutputQ: "OOP_ENDPOINTS_HTTP_OUTPUT_Q",
     requestTimeout: "OOP_REQUEST_TIMEOUT",
