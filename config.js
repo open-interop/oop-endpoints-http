6ea4e5b87fc69e77e8dd2545057df6b5e4c1fc90
Daniel Young <daniel.young@bluefrontier.co.uk>
2020-07-22 09:37:14 +0100

    fix: readding config var

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const oop = require("oop-node-common");
 module.exports = new oop.Config({
     amqpAddress: "OOP_AMQP_ADDRESS",
     exchangeName: "OOP_EXCHANGE_NAME",
+    endpointsExchangeName: "OOP_ENDPOINTS_EXCHANGE_NAME",
     oopEndpointsQ: "OOP_ENDPOINT_Q",
     httpOutputQ: "OOP_ENDPOINTS_HTTP_OUTPUT_Q",
     requestTimeout: "OOP_REQUEST_TIMEOUT",
